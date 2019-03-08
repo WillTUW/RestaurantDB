ee4169f0e2e615e3d199d4e2c710e60a5f48e3c6
tarcisiusdaniel <46510062+tarcisiusdaniel@users.noreply.github.com>
2019-03-08 03:28:12 -0800

    Update 475ProjectIteration1.sql

--- 475ProjectIteration1.sql
+++ 475ProjectIteration1.sql
@@ -35,10 +35,8 @@ CREATE TABLE REVIEW
     rID INT UNIQUE NOT NULL,
     review_day DATE NOT NULL,
     message TEXT,
-    FOREIGN KEY(uID) REFERENCES rUser
-
-(userID),
-    FOREIGN KEY(rID) REFERENCES RESTUARANT(rID)
+    FOREIGN KEY(uID) REFERENCES RUSER(userID),
+    FOREIGN KEY(rID) REFERENCES RESTAURANT(rID)
 );
 
 CREATE TABLE RESTAURANT
@@ -226,23 +224,23 @@ insert into MENU (m_name, r_id, LANGUAGE) values ('IPromiseItIsFresh4', 12, 'Eng
 ------------------- BEGINNING OF FOOD_ENTRIES INSERTIONS-------------------
 INSERT INTO FOOD_ENTRIES
 values(
-    "$10.00", "Tamales", 1, "Lunch"
+    10.00, "Tamales", 1, "Lunch"
 );
 INSERT INTO FOOD_ENTRIES
 values(
-    "$12.00", "Tacos", 1, "Lunch"
+    12.00, "Tacos", 1, "Lunch"
 );
 INSERT INTO FOOD_ENTRIES
 values(
-    "$3.00", "Pork", 1, "Lunch"
+    3.00, "Pork", 1, "Lunch"
 );
 INSERT INTO FOOD_ENTRIES
 values(
-    "$6.00", "Tortillas", 1, "Lunch"
+    6.00, "Tortillas", 1, "Lunch"
 );
 INSERT INTO FOOD_ENTRIES
 values(
-    "$30.00", "Steak", 1, "Lunch"
+    30.00, "Steak", 1, "Lunch"
 );
 ------------------- END OF FOOD_ENTRIES INSERTIONS------------------------
 
@@ -290,18 +288,49 @@ CITY.name = RESTAURANT.rCity;
 -- TODO Display cheapest food
 SELECT *
 FROM FOOD_ENTRIES
-ORDER BY FOOD_ENTRIES.price ASC;
+WHERE FOOD_ENTRIES.price = (SELECT MIN(price) FROM FOOD_ENTRIES);
 
 -- TODO Display most expensive food
-
+SELECT *
+FROM FOOD_ENTRIES
+WHERE FOOD_ENTRIES.price = (SELECT MAX(price) FROM FOOD_ENTRIES);
+         
 -- TODO Display the most expensive food in each enthic catagory
+SELECT *
+FROM FOOD_ENTRIES
+WHERE FOOD_ENTRIES.price IN(SELECT MAX(price) 
+                            FROM FOOD_ENTRIES, MENU, RESTAURANT 
+                            WHERE r_menu_id = r_id AND r_id = rID
+                            GROUP BY cuisine)
+ORDER BY price ASC;
 
 -- TODO Display the least expensive food in each ethnic catagory
-
+SELECT *
+FROM FOOD_ENTRIES
+WHERE FOOD_ENTRIES.price IN(SELECT MIN(price) 
+                            FROM FOOD_ENTRIES, MENU, RESTAURANT 
+                            WHERE r_menu_id = r_id AND r_id = rID
+                            GROUP BY cuisine)
+ORDER BY price ASC;
+         
 -- TODO Display the cheapest food in each town
+SELECT *
+FROM FOOD_ENTRIES
+WHERE FOOD_ENTRIES.price IN(SELECT MIN(price) 
+                            FROM FOOD_ENTRIES, MENU, RESTAURANT, CITY
+                            WHERE r_menu_id = r_id AND r_id = rID AND rCity = CITY.name
+                            GROUP BY CITY.name)
+ORDER BY price ASC;
 
 -- TODO Display the most expensive food in each town 
-
+SELECT *
+FROM FOOD_ENTRIES
+WHERE FOOD_ENTRIES.price IN(SELECT MAX(price) 
+                            FROM FOOD_ENTRIES, MENU, RESTAURANT, CITY
+                            WHERE r_menu_id = r_id AND r_id = rID AND rCity = CITY.name
+                            GROUP BY CITY.name)
+ORDER BY price ASC;
+         
 -- TODO Display highest rated restaurant in each CITY 
 
 -- TODO Display highest rated restaurant in each state
